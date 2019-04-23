8dbcc5ec5e21b0e29da58ed56b2f8a4325d8874c
Ivo Jimenez <ivo.jimenez@gmail.com>
2019-04-23 15:53:14 -0700

    adds slice name to script

--- .ci/one-baremetal-node.py
+++ .ci/one-baremetal-node.py
@@ -3,6 +3,7 @@ from geni.rspec import pg
 from geni import utils
 
 # create request
+# {
 node = pg.RawPC("node")
 
 node.disk_image = "urn:publicid:IDN+clemson.cloudlab.us+image+schedock-PG0:ubuntu18-docker"
@@ -10,8 +11,13 @@ node.hardware_type = 'c6320'
 
 request = pg.Request()
 request.addResource(node)
+# }
 
 # load context
 ctx = utils.loadContext()
 
-cloudlab.Clemson
+# create slice
+utils.createSlice(ctx, 'popperized')
+
+# create sliver on clemson
+utils.createSliver(ctx, cloudlab.Clemson, 'popperized', request)
